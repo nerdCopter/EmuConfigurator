a6dabe87e31412dc233a9dea7c9a79bf1aa15d22
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2020-04-10 16:13:40 -0500

    WIP; testing

---
+++ .travis/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# travis continuous integration build script for
+# EmuConfigurator
+
+## build
+
+# install dependencies
+yarn --frozen-lockfile || exit $?
+yarn gulp clean-release
+
+# build releases for each platform
+case "${TRAVIS_OS_NAME}" in
+    linux)
+        yarn gulp release --chromeos
+        yarn gulp release --linux64
+        ;;
+    osx)
+        yarn gulp release --osx64
+        ;;
+    windows)
+        yarn gulp mrelease --win32
+        yarn gulp mrelease --win64
+        ;;
+    *)
+        echo "platform ${TRAVIS_OS_NAME} not supported for now."
+        exit 2
+        ;;
+esac
+
+ls -lhrt release/
+
+# get version string from 'package.json'
+export CONFIGURATOR_VERSION="$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')"
+
+# compose string to reference the package
+export PACKAGE_VERSION="${CONFIGURATOR_VERSION}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_OS_NAME}"
+
+exit 0
---
+++ .travis/push.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b travis
+  git add . release/*
+  git commit --message "Travis build: $PACKAGE_VERSION"
+}
+
+upload_files() {
+  ls -lhrt release/*
+  git remote add origin-upload https://${nctrb}@github.com/nerdCopter/travisBuilds.git > /dev/null 2>&1
+  git remote add origin-upload https://${PAT}@github.com/nerdCopter/travisBuilds.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-upload travis
+}
+
+setup_git
+commit_website_files
+upload_files
