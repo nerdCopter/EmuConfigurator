2a8ac5c0d1fa18726ddcc7b354e478cecbdbbafd
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2020-12-12 12:25:03 -0600

    WIP testing SkyLion presets only; not mergeable

--- src/js/main.js
+++ src/js/main.js
@@ -38,7 +38,7 @@ var helioUrlv030 = "https://raw.githubusercontent.com/emuflight/emuflight-preset
 
 //testing only
 var nonHelioUrlv030 = "https://raw.githubusercontent.com/nerdCopter/emuflight-presets_nerdRepo/20201211_SkyLion_0.3.2_presets/presets-0.3.0/presets-nonHELIO.json";
-var helioUrlv030 = "https://raw.githubusercontent.com/nerdCopter/emuflight-presets_nerdRepo/20201211_SkyLion_0.3.2_presets/presets-0.3.0/presets-HELIO.json";
+var helioUrlv030    = "https://raw.githubusercontent.com/nerdCopter/emuflight-presets_nerdRepo/20201211_SkyLion_0.3.2_presets/presets-0.3.0/presets-HELIO.json";
 
 
 // TODO: migrate to a function to get rid of code duplication
